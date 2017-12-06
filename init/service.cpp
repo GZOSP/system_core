8b4c96ad9188cc394f5d70dd37a1e651ba33fed9
theringmaster <gzroms@hotmail.com>
2017-12-06 14:26:54 -0500

    Revert "init: don't skip starting a service with no domain if permissive"
    This reverts commit 27279f1c91832a6fbb5f4b4394a1bcfd3e452c6e.

--- init/service.cpp
+++ init/service.cpp
@@ -85,9 +85,7 @@ static std::string ComputeContextFromExecutable(std::string& service_name,
     }
     if (rc == 0 && computed_context == mycon.get()) {
         LOG(ERROR) << "service " << service_name << " does not have a SELinux domain defined";
-        if (selinux_status_getenforce() > 0) {
-            return "";
-        }
+        return "";
     }
     if (rc < 0) {
         LOG(ERROR) << "could not get context while starting '" << service_name << "'";
